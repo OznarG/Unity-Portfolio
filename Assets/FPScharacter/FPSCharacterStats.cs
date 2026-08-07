b2350118dfbf895605e54a71f7c501ab021d953d
Angel Joel Garcia Mordan <AJGarciaMordan@student.FullSail.edu>
2026-08-07 07:30:14 -0400

    Fixed Script Visual Studio Issue

--- Assets/FPScharacter/FPSCharacterStats.cs
+++ Assets/FPScharacter/FPSCharacterStats.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class FPSCharacterStats : MonoBehaviour
 {
+    float speed;
+    bool jumping;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
