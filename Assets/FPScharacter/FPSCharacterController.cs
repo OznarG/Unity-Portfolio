a2285edf189326b378faf2fc85faf1a81b3fab99
Angel Joel Garcia Mordan <AJGarciaMordan@student.FullSail.edu>
2026-08-09 22:18:57 -0400

    Added Player Capsule
    -Added Rigitbody and locked rotation on x and z axis, and moved the camera into player sphere
    
    -Added ground tag to ground
    
    -Added Player tag to sphere
    
    -Added Jump and Ground check to stats and controller scripts
    
    -Added InputMapSystem and Created playerInput On Foot Map

--- Assets/FPScharacter/FPSCharacterController.cs
+++ Assets/FPScharacter/FPSCharacterController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FPSCharacterController : MonoBehaviour
 {
+    [SerializeField] FPSCharacterStats fpsStats;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private bool isGrounded;
+    private PlayerInput playerInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        fpsStats = GetComponent<FPSCharacterStats>();
+        rb = GetComponent<Rigidbody>();
+        playerInput = new PlayerInput();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckGround();
+    }
 
+    void OnJump()
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(new Vector3(0, fpsStats.jumpForce, 0), ForceMode.Impulse);
+        }
+
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(fpsStats.groundCheck.position, fpsStats.groundDistance, fpsStats.groundMask);
     }
 }
--- Assets/FPScharacter/FPSCharacterStats.cs
+++ Assets/FPScharacter/FPSCharacterStats.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FPSCharacterStats : MonoBehaviour
 {
-    float speed;
-    bool jumping;
+    public float speed;
+    public float jumpForce;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
