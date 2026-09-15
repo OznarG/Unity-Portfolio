ff5e137f1adedd7ea2a4cd663f532ea7834a4dd5
Angel Joel Garcia Mordan <AJGarciaMordan@student.FullSail.edu>
2026-09-14 22:01:11 -0400

    Added Behaviour tree logic

--- Assets/Scripts/Enemies/_UnityEnemySystem/LineOfSight.cs
+++ Assets/Scripts/Enemies/_UnityEnemySystem/LineOfSight.cs
@@ -14,7 +14,7 @@ public class LineOfSight : MonoBehaviour
         Vector3 direction = potentialTarget.transform.position - transform.position; 
         Physics.Raycast(transform.position + Vector3.up * m_detectionHeight, direction, out hit, m_detectionRange, m_playerLayerMask);
 
-        if(hit.collider != null && hit.collider.gameObject == potentialTarget)
+        if(hit.collider != null /*&& hit.collider.gameObject == potentialTarget*/)
         {
             if(showSebugVisuals && this.enabled)
             {
