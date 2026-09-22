81d1672db37d7ce4b885b6625f61cff99389ce03
Angel Joel Garcia Mordan <AJGarciaMordan@student.FullSail.edu>
2026-09-22 07:00:41 -0400

    Change rendering pipeline

--- Assets/Scripts/Interfaces/Iinteractor.cs
+++ Assets/Scripts/Interfaces/Iinteractor.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class Iinteractor : MonoBehaviour
+public interface Iinteractor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    abstract void ReadyToInteract();
+    abstract void Interact();
 }
